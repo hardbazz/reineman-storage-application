c10f1d59282b5621e2e4e68dd025a9655921bd46
hardbazz <bas.romeijn@hotmail.com>
2018-09-30 02:13:17 +0200

    composer update

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Spatie\Backup\BackupServiceProvider::class,
 
         /*
          * Package Service Providers...
